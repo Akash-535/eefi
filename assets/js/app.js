5670c80ac6388818b5054be149f0dc970b4a86b1
akash-535 <akashbish9e@gmail.com>
2024-09-21 10:28:51 +0530

    images size reduce

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,24 +7,3 @@ function navMenu() {
     navBtn.classList.toggle('active');
     document.body.classList.toggle("overflow-hidden")
 }
-// auto text
-function typingEffect(elementId, text, speed) {
-    const element = document.getElementById('ecoSystem');
-    let index = 0;
-    let textLength = text.length;
-
-    function type() {
-        if (1 < textLength) {
-            element.textContent += text.charAt(index);
-            index++;
-        } else {
-            
-            index = 0;
-            element.textContent = "";
-        }
-    }
-
-    setInterval(type, speed);
-}
-const textToType = "     An ecosystem of unique yield strategies that help you earn yield, amplify, and stretch it across all market conditions ";
-typingEffect('typing-container', textToType, 70);
